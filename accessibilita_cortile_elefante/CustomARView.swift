1594c3aa377efbec847a72d46acefda40da009a1
Giacomo-Papaluca <giacomo.papaluca@gmail.com>
2021-10-10 13:26:30 +0200

    Refactoring: better colored square creation

--- accessibilita_cortile_elefante/CustomARView.swift
+++ accessibilita_cortile_elefante/CustomARView.swift
@@ -143,16 +143,14 @@ class CustomARView: UIViewController, ARSessionDelegate {
                     break
                 case "greenSquare":
                     let planeMesh = MeshResource.generatePlane(width: 0.3, depth: 0.3)
-                    var planeMaterial = UnlitMaterial()
-                    planeMaterial.baseColor = MaterialColorParameter.color(.green)
+                let planeMaterial = SimpleMaterial(color: .green, isMetallic: false)
                     let planeModel = ModelEntity(mesh: planeMesh, materials: [planeMaterial])
                     adjustModelEntity(planeModel, anchor)
                     anchorEntity.addChild(planeModel)
                     break
                 case "redSquare":
                     let planeMesh = MeshResource.generatePlane(width: 0.3, depth: 0.3)
-                    var planeMaterial = UnlitMaterial()
-                    planeMaterial.baseColor = MaterialColorParameter.color(.red)
+                let planeMaterial = SimpleMaterial(color: .red, isMetallic: false)
                     let planeModel = ModelEntity(mesh: planeMesh, materials: [planeMaterial])
                     adjustModelEntity(planeModel, anchor)
                     anchorEntity.addChild(planeModel)
