e33c1a0515e50504f0e1996da87daf51f1540ad0
Giacomo-Papaluca <giacomo.papaluca@gmail.com>
2021-10-13 12:01:33 +0200

    Refactoring: added alert when map saved, conformed planeMaterial creation to ios15

--- accessibilita_cortile_elefante/CustomARView.swift
+++ accessibilita_cortile_elefante/CustomARView.swift
@@ -177,6 +177,7 @@ class CustomARView: UIViewController, ARSessionDelegate {
                 }
             }
         }
+        showAlert(title: "MAP SAVED!", message: "AR World Map successfully saved")
     }
     
     @IBAction func SalvaDidTap(_ sender: Any) {
@@ -233,7 +234,7 @@ class CustomARView: UIViewController, ARSessionDelegate {
         case "greenSquare":
             
             let planeMesh = MeshResource.generatePlane(width: 0.3, depth: 0.3)
-            let planeMaterial = SimpleMaterial(color: .green, isMetallic: false)
+            let planeMaterial = SimpleMaterial(color: .green, roughness: MaterialScalarParameter(floatLiteral: 1), isMetallic: false)
             let planeModel = ModelEntity(mesh: planeMesh, materials: [planeMaterial])
                         
             addModel(planeModel, to: anchorEntity, mappedWith: arAnchor)
@@ -241,7 +242,7 @@ class CustomARView: UIViewController, ARSessionDelegate {
             break
         case "redSquare":
             let planeMesh = MeshResource.generatePlane(width: 0.3, depth: 0.3)
-            let planeMaterial = SimpleMaterial(color: .red, isMetallic: false)
+            let planeMaterial = SimpleMaterial(color: .red, roughness: MaterialScalarParameter(floatLiteral: 1), isMetallic: false)
             let planeModel = ModelEntity(mesh: planeMesh, materials: [planeMaterial])
                         
             addModel(planeModel, to: anchorEntity, mappedWith: arAnchor)
@@ -319,14 +320,14 @@ class CustomARView: UIViewController, ARSessionDelegate {
                     break
                 case "greenSquare":
                     let planeMesh = MeshResource.generatePlane(width: 0.3, depth: 0.3)
-                let planeMaterial = SimpleMaterial(color: .green, isMetallic: false)
+                let planeMaterial = SimpleMaterial(color: .green, roughness: MaterialScalarParameter(floatLiteral: 1), isMetallic: false)
                     let planeModel = ModelEntity(mesh: planeMesh, materials: [planeMaterial])
                     adjustModelEntity(planeModel, anchor)
                     anchorEntity.addChild(planeModel)
                     break
                 case "redSquare":
                     let planeMesh = MeshResource.generatePlane(width: 0.3, depth: 0.3)
-                    let planeMaterial = SimpleMaterial(color: .red, isMetallic: false)
+                    let planeMaterial = SimpleMaterial(color: .red, roughness: MaterialScalarParameter(floatLiteral: 1), isMetallic: false)
                     let planeModel = ModelEntity(mesh: planeMesh, materials: [planeMaterial])
                     adjustModelEntity(planeModel, anchor)
                     anchorEntity.addChild(planeModel)
